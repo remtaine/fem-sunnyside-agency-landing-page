bc5490b53602191db6abdf212b1da1bcfc285f93
Niel Benjamin Kho <benjokho@gmail.com>
2022-07-18 08:52:14 +0800

    Add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "components/Header";
+import React from "react";
+import Footer from "./components/Footer";
+import ImageGallery from "./components/ImageGallery";
+import Navbar from "./components/Navbar";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-w-screen min-h-screen bg-fem-50 font-barlow font-semibold selection:bg-fem-600 selection:text-fem-50">
+      <Navbar />
+      <Header />
+      <main className="">
+        <Testimonials />
+        <ImageGallery />
+      </main>
+      <Footer />
     </div>
   );
 }
