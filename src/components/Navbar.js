6f91461ae983d3393250602389eaf0df4b294add
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-09 20:59:25 +0800

    Add underline links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,35 @@
 import React from "react";
-import Logo from "./svg/Logo";
+import Logo from "./ui/Logo";
+import UnderlineLink from "./ui/UnderlineLink";
 
 function Navbar() {
   return (
     <nav className="absolute flex h-fit w-full items-center justify-between p-3 font-semibold">
-      <div className="scale-125 pl-4">
+      <a href="" className="scale-125 pl-4 duration-400 hover:scale-[135%]">
         <Logo />
-      </div>
+      </a>
       <div className="flex h-fit w-fit items-center justify-center gap-4 text-fem-50">
-        <button className="px-2 py-1 text-xl md:hidden">
-          <i class="fa-solid fa-bars"></i>
+        <button className="block px-2 py-1 text-xl duration-400 hover:text-neutral-300 md:hidden">
+          <i className="fa-solid fa-bars"></i>
         </button>
-        <div className="">
-          <a
-            href=""
-            className="decoration-[3px] underline-offset-2 hover:underline"
-          >
+        {/* TODO collapsible menu buttons */}
+        <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+          <UnderlineLink link="#" bgColor="bg-fem-100">
             About
+          </UnderlineLink>
+          <UnderlineLink link="#" bgColor="bg-fem-200">
+            Services
+          </UnderlineLink>
+          <UnderlineLink link="#" bgColor="bg-fem-400">
+            Project
+          </UnderlineLink>
+          <a
+            href="#"
+            className="rounded-3xl bg-fem-50 py-2 px-3 text-fem-600 duration-400 hover:bg-[#6ECEFF] hover:text-fem-50"
+          >
+            Contact
           </a>
-          <div className=""></div>
         </div>
-        <a
-          href=""
-          className="decoration-[3px] underline-offset-2 hover:underline"
-        >
-          Services
-        </a>
-        <a
-          href=""
-          className="decoration-[3px] underline-offset-2 hover:underline"
-        >
-          Project
-        </a>
-        <a
-          href=""
-          className="rounded-3xl bg-fem-50 py-2 px-3 text-fem-600 duration-400 hover:bg-[#6ECEFF] hover:text-fem-50"
-        >
-          Contact
-        </a>
       </div>
     </nav>
   );
