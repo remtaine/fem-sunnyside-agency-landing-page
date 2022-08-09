a2082ecefcfdb3487cee7f791620e6bb40f2b711
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-09 22:49:24 +0800

    Make mobile navbar button work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./ui/Logo";
 import UnderlineLink from "./ui/UnderlineLink";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const toggleMenuOpen = function () {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="absolute flex h-fit w-full items-center justify-between p-3 font-semibold">
       <a href="" className="scale-125 pl-4 duration-400 hover:scale-[135%]">
         <Logo />
       </a>
-      <div className="flex h-fit w-fit items-center justify-center gap-4 text-fem-50">
-        <button className="block px-2 py-1 text-xl duration-400 hover:text-neutral-300 md:hidden">
+      <div className="relative flex h-fit w-fit items-center justify-center gap-4 text-fem-50">
+        <button
+          onClick={toggleMenuOpen}
+          className={
+            "block px-2 py-1 text-xl duration-400 hover:text-neutral-300 md:hidden " +
+            (isMenuOpen ? "opacity-100" : "opacity-50")
+          }
+        >
           <i className="fa-solid fa-bars"></i>
         </button>
         {/* TODO collapsible menu buttons */}
-        <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+        <div
+          className={
+            "fixed top-20 right-0 z-50 mr-5 w-fit flex-col items-center justify-center gap-4 bg-fem-50 px-20 py-10 font-barlow font-normal text-fem-700 shadow md:relative md:top-0 md:flex md:flex-row md:bg-transparent md:p-0 md:text-inherit md:shadow-none " +
+            (isMenuOpen ? "hidden" : "flex")
+          }
+        >
           <UnderlineLink link="#" bgColor="bg-fem-100">
             About
           </UnderlineLink>
@@ -25,7 +41,7 @@ function Navbar() {
           </UnderlineLink>
           <a
             href="#"
-            className="rounded-3xl bg-fem-50 py-2 px-3 text-fem-600 duration-400 hover:bg-[#6ECEFF] hover:text-fem-50"
+            className="rounded-3xl bg-fem-200 py-2 px-3 text-fem-600 duration-400 hover:bg-[#6ECEFF] hover:text-fem-50 md:bg-fem-50"
           >
             Contact
           </a>
