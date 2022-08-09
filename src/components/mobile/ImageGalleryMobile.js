8b0a4aed8840ca3705d969c2afb76955ef97eb0b
Niel Benjamin Kho <benjokho@gmail.com>
2022-08-09 21:00:26 +0800

    Add image gallery component

---
+++ src/components/mobile/ImageGalleryMobile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import MobileCone from "images/mobile/image-gallery-cone.jpg";
+import MobileBottles from "images/mobile/image-gallery-milkbottles.jpg";
+import MobileOrange from "images/mobile/image-gallery-orange.jpg";
+import MobileSugar from "images/mobile/image-gallery-sugar-cubes.jpg";
+
+function ImageGalleryMobile() {
+  return (
+    <section className="max-w-screen block grid grid-cols-2 md:hidden">
+      <img src={MobileBottles} alt="" className="w-full" />
+      <img src={MobileOrange} alt="" className="w-full" />
+      <img src={MobileCone} alt="" className="w-full" />
+      <img src={MobileSugar} alt="" className="w-full" />
+    </section>
+  );
+}
+
+export default ImageGalleryMobile;
